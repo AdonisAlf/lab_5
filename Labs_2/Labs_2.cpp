34f3b799bfe3ce2a9925066ced6417fbe5ccef40
sono109 <dallkil012@gmail.com>
2022-11-29 00:33:59 +0300

    Initial commit

---
+++ Labs_2/Labs_2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void checkValidInput() {//checking for valid input
+    if (cin.fail()) {
+        throw "Incorrect input";
+    }
+}
+
+void checkValidParams(int n, int x) { //checking for valid params
+    if (x < 0)
+    {
+        if (n < 2) {
+            throw "Input correct data";
+        }
+    }
+    else
+    {
+        if (n < 1) {
+            throw "Input correct data";
+        }
+    }
+}
+
+int first(int n, int x) //first part of eq
+{
+    int p = 1;
+    for (int j = 2; j <= n - 2; j++)
+    {
+        p *= j + 1;
+    }
+    return (p);
+}
+int second(int n, int x) //second part of eq
+{
+    int sum = 0;
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int p = 1;
+        for (int j = 0; j <= n - 1; j++)
+        {
+            p *= x + i + j * j;
+        }
+        sum += p;
+    }
+    return (sum);
+}
+
+int y(int n, int x)//calculation
+{
+    if (x < 0)
+    {
+        return (first(n, x));
+    }
+    else
+    {
+        return (second(n, x));
+    }
+}//Продовження на наступній сторінці
+
+int main()
+{
+    int n, x = { 0 };
+    try {
+        cout << "Enter n & x: ";
+        cin >> n >> x;
+        //checking for errors and invalid parametrs
+        checkValidParams(n, x);
+        checkValidInput();
+        cout << "y = " << y(n, x);
+    }
+    catch (const char* ex) {
+        cout << ex << endl;
+        return -1;
+    }
+    catch (...) {
+        cout << "Unknown error";
+    }
+}
---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "H:\project\Харьков\BIPS\Labs_2\Labs_2\Labs_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(calculate_Tests)
+	{
+	public:
+		TEST_METHOD(calculate7_and_minus1_24rtn)
+		{
+			int n = 7;
+			int x = -1;
+			int expec = 24;
+			int actual = y(n, x);
+			Assert::AreEqual(expec, actual);
+		}
+
+	public:
+		TEST_METHOD(calculate3_and_5_1306rtn)
+		{
+			int n = 3;
+			int x = 5;
+			int expec = 1306;
+			int actual = y(n, x);
+			Assert::AreEqual(expec, actual);
+		}
+	};
+
+	TEST_CLASS(checkValidParams_Tests)
+	{
+	public:
+		TEST_METHOD(checkValidParams_minus100) 
+		{
+			int n = -100;
+			int x = -100;
+			try
+			{
+				checkValidParams(n, x);
+				Assert::Fail();
+			}
+			catch (...) {
+				Assert::IsTrue(true);
+			}
+		}
+	};
+}
+
+
